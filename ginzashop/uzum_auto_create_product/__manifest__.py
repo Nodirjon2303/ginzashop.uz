ed45f02c5a76c03d697791592c4b86f83d3ce058
Nodirjon <ruzimurodovnodir66@gmail.com>
2024-08-16 22:35:09 +0500

    initial

---
+++ ginzashop/uzum_auto_create_product/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Uzum Auto Create Product',
+    "summary": "Auto create product from uzum",
+    "author": "Ruzimurodov Nodirjon",
+    'version': '0.1',
+    'depends': ['base', 'web', "website_sale", "stock"],
+    'data': [
+    ],
+    'assets': {
+        'web.assets_backend': [
+        ],
+    },
+    'installable': True,
+    'application': True,
+}
---
+++ run.sh
@@ -0,0 +1,1 @@
+/Users/admin/Desktop/Projects/UIC/odoo/base/17/env/bin/python3 /Users/admin/Desktop/Projects/UIC/odoo/base/17/odoo/odoo-bin -c odoo.conf
